a3b6a46e773ddb36c85c69ec107d1239d98abadb
GabrielCStabile <devstabile@gmail.com>
2022-12-06 09:44:53 -0300

    Adicionando Swagger ao projeto

--- src/main/java/br/com/apiprodutos/controller/ProdutoController.java
+++ src/main/java/br/com/apiprodutos/controller/ProdutoController.java
@@ -4,10 +4,7 @@ import br.com.apiprodutos.entity.Produto;
 import br.com.apiprodutos.service.ProdutoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,9 +23,16 @@ public class ProdutoController {
         return ResponseEntity.ok(produtoService.buscarTodos());
     }
 
+    @PostMapping
     public ResponseEntity <Produto> salvar(@RequestBody Produto produto){
         var novoProduto = produtoService.salvar(produto);
         return new ResponseEntity(produto, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity <Void> excluir (@PathVariable Long id){
+        produtoService.delete(id);
+        return ResponseEntity.ok().build();
     }
 
 
--- src/main/java/br/com/apiprodutos/entity/Produto.java
+++ src/main/java/br/com/apiprodutos/entity/Produto.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public class Produto {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
